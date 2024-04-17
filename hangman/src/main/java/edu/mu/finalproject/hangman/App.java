af391e9bb1e03c5757bb3eb3e2ef5b154bf7b2e3
TryConor <cjmw5t@umsystem.edu>
2024-04-17 16:51:05 -0500

    Maven Project Creation
    Created a Maven Project in Eclipse

---
+++ hangman/src/main/java/edu/mu/finalproject/hangman/App.java
@@ -0,0 +1,9 @@
+package edu.mu.finalproject.hangman;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+	
+}
---
+++ hangman/src/test/java/edu/mu/finalproject/hangman/testing/AppTest.java
@@ -0,0 +1,12 @@
+package edu.mu.finalproject.hangman.testing;
+
+import org.testng.annotations.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		assert Boolean.TRUE;
+	}
+	
+}
