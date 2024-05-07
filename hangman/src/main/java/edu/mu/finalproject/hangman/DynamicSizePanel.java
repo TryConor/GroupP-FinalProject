6d158f64df1c435da6d91487d07c165ec8404910
kriswizzle03 <krislmor21@gmail.com>
2024-05-06 22:09:24 -0500

    comments for clarity

--- hangman/src/main/java/edu/mu/finalproject/hangman/DynamicSizePanel.java
+++ hangman/src/main/java/edu/mu/finalproject/hangman/DynamicSizePanel.java
@@ -6,10 +6,19 @@ import java.awt.Dimension;
 
 import javax.swing.JPanel;
 
+//A JPanel that dynamically adjusts its preferred size to fill the entire screen.
+
 public class DynamicSizePanel extends JPanel {
-	public DynamicSizePanel() {
+
+ DynamicSizePanel() {
 		
     }
+ 
+ /* 
+ Overrides the getPreferredSize method to return the dimensions of the screen.
+ The preferred size of the panel, equal to the screen dimensions.
+  */
+ 
 	@Override
     public Dimension getPreferredSize() {
         Dimension screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
