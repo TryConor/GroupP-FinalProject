b1aba8ab0f0c74563fc7d6fd096d7e8f31d2619d
Eli Wuerth <emwcd7@umsystem.edu>
2024-05-06 21:28:32 -0500

    populateLevelComboBox

--- hangman/src/main/java/edu/mu/finalproject/hangman/StartMenu.java
+++ hangman/src/main/java/edu/mu/finalproject/hangman/StartMenu.java
@@ -5,6 +5,11 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -12,6 +17,24 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
+import edu.mu.Game.Database;
+
 public class StartMenu extends JFrame{
+	
+	
+	void populateLevelComboBox(Database database) {
+		levelComboBox.removeAllItems();
+		levelComboBox.addItem("Select a level");
+		// if categories index is greater than 0, it reads the selected category and
+		// looks to see how many levels it has and adds them to the drop down
+		if (categoryComboBox.getSelectedIndex() > 0) {
+			String selectedCategory = (String) categoryComboBox.getSelectedItem();
+			Map<String, List<String>> selectedCategoryMap = database.getCategories().get(selectedCategory);
+			List<String> levels = new ArrayList<>(selectedCategoryMap.keySet());
+			for (String level : levels) {
+				levelComboBox.addItem(level);
+			}
+		}
+	}
 	
 }
