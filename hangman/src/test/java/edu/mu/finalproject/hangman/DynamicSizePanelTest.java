2d440eb6a8d1fe0c974f57e0fa4ac91ecb37edf8
MichaelKeeney <112013713+MichaelKeeney@users.noreply.github.com>
2024-05-06 22:22:44 -0500

    TestDynamicSizePanel

--- hangman/src/test/java/edu/mu/finalproject/hangman/DynamicSizePanelTest.java
+++ hangman/src/test/java/edu/mu/finalproject/hangman/DynamicSizePanelTest.java
@@ -1,34 +1,54 @@
 package edu.mu.finalproject.hangman;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.awt.Cursor;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.util.Locale;
+import java.util.MissingResourceException;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-class DynamicSizePanelTest {
+class DynamicSizePanelDiffblueTest {
+    /**
+     * Method under test: {@link DynamicSizePanel#getPreferredSize()}
+     */
+    @Test
+    @Disabled("TODO: Complete this test")
+    void testGetPreferredSize() {
+        // TODO: Diffblue Cover was only able to create a partial test for this method:
+        //   Diffblue AI was unable to find a test
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
+        // Arrange
+        // TODO: Populate arranged inputs
+        DynamicSizePanel dynamicSizePanel = null;
 
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
+        // Act
+        Dimension actualPreferredSize = dynamicSizePanel.getPreferredSize();
 
-	@BeforeEach
-	void setUp() throws Exception {
-	}
+        // Assert
+        // TODO: Add assertions on result
+    }
 
-	@AfterEach
-	void tearDown() throws Exception {
-	}
+    /**
+     * Method under test: default or parameterless constructor of
+     * {@link DynamicSizePanel}
+     */
+    @Test
+    void testNewDynamicSizePanel() throws MissingResourceException {
+        // Arrange and Act
+        DynamicSizePanel actualDynamicSizePanel = new DynamicSizePanel();
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
-
+        // Assert
+        Locale locale = actualDynamicSizePanel.getLocale();
+        assertEquals("", locale.getDisplayScript());
+        assertEquals("", locale.getDisplayVariant());
+        assertEquals("", locale.getScript());
+        assertEquals("", locale.getVariant());
+        Cursor cursor = actualDynamicSizePanel.getCursor();
+        assertEquals("Default Cursor", cursor.getName());
+        Font font = actualDynamicSizePanel.getFont();
+        
+        
+    }
 }
