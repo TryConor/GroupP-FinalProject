3969f381119b7acd5cca005d7c6b76eb5d86644b
TryConor <cjmw5t@umsystem.edu>
2024-05-06 22:40:51 -0500

    hangman test cases
    hangman test cases

--- hangman/src/test/java/edu/mu/finalproject/hangman/HangmanTest.java
+++ hangman/src/test/java/edu/mu/finalproject/hangman/HangmanTest.java
@@ -1,34 +1,75 @@
 package edu.mu.finalproject.hangman;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import java.io.IOException;
+
+import javax.swing.JButton;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+
 class HangmanTest {
+    @Test
+    @Disabled("TODO: Complete this test")
+    void testNewHangman() throws IOException {
+        // Arrange and Act
+        Hangman newman = new Hangman("janedoe", 1, 1);
+        assertEquals(newman,new Hangman("janedoe", 1, 1) );
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
+    }    
+    @Test
+    public void testConstructor() throws IOException {
+        String username = "JohnDoe";
+        int totalWins = 5;
+        int totalLosses = 3;
+        Hangman hangman = new Hangman(username, totalWins, totalLosses);
+    }
+    
+    @Test
+    public void testButtonClickIncorrectGuess() throws IOException {
+        Hangman hangman = new Hangman("JohnDoe", 0, 0);
+        JButton button = hangman.X;
+        button.doClick();
+    }
+    
+    @Test
+    public void testGameOverWin() throws IOException {
+        Hangman hangman = new Hangman("JohnDoe", 0, 0);
+    }
+    
+    @Test
+    public void testGameOverLoss() throws IOException {
+        Hangman hangman = new Hangman("JohnDoe", 0, 0);
+    }
 
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
+@Test
+    public void testCorrectGuessActionListener() throws IOException {
+        Hangman hangman = new Hangman("JohnDoe", 0, 0);
+    }
 
-	@BeforeEach
-	void setUp() throws Exception {
-	}
+    @Test
+    public void testIncorrectGuessActionListener() throws IOException {
+        Hangman hangman = new Hangman("JohnDoe", 0, 0);
+    }
 
-	@AfterEach
-	void tearDown() throws Exception {
-	}
+    @Test
+    public void testButtonDisablingActionListener() throws IOException {
+        Hangman hangman = new Hangman("JohnDoe", 0, 0);
+        // Simulate clicking the 'A' button
+        // Check if 'A' button is disabled after click
+        assertFalse(hangman.A.isEnabled());
+    }
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testGameOverWinActionListener() throws IOException {
+        Hangman hangman = new Hangman("JohnDoe", 0, 0);
+    }
+
+    @Test
+    public void testGameOverLossActionListener() throws IOException {
+        Hangman hangman = new Hangman("JohnDoe", 0, 0);
+    }
 
 }
